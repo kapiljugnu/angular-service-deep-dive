7786949b31b0af8a583bfd6086f498667560b5ce
Kapil Jugnu <kr_kapil@hotmail.com>
2025-02-28 14:24:29 +0530

    custom injector with value instead of class

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,4 +1,38 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+
+export type TaskStatusOptionsType = {
+  value: 'open'|'in-progress'|'done';
+  taskStatus: TaskStatus,
+  text: string;
+};
+
+export const TASK_STATUS_OPTIONS =  new InjectionToken<TaskStatusOptionsType>('task-status-options');
+
+export const TaskStatusOptions: TaskStatusOptionsType[] = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed'
+  }
+]
+
+
+export const TaskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOptions
+} 
 
 export interface Task {
   id: string;
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, signal, inject, computed } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
+import { TASK_STATUS_OPTIONS, TaskStatusOptionsProvider, TaskStatusOptionsType } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,10 +10,13 @@ import { TasksService } from '../tasks.service';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [TaskStatusOptionsProvider]
 })
 export class TasksListComponent {
   private taskService = inject(TasksService);
   private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject<TaskStatusOptionsType[]>(TASK_STATUS_OPTIONS);
+
   tasks = computed(() => {
     switch(this.selectedFilter()) {
       case 'open': 
